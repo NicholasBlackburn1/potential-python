31976dd7f1eef85d1a0f12a2ae86f7cc0eb04346
nickthehacker <nickblackburn02@gmail.com>
2018-07-15 16:22:31 -0400

    added

--- items.py
+++ items.py
@@ -12,5 +12,30 @@ class Phone(Item):
     def __init__(self, amt):
         self.amt = amt
         super().__init__(name="Cell-Phone",
-                         description=" {} stamped on the front.".format(str(self.amt)),
+                         description="Your SmartPhone.".format(str(self.amt)),
                          value=self.amt)
+
+
+class Weapon(Item):
+    def __init__(self, name, description, value, damage):
+        self.damage = damage
+        super().__init__(name, description, value)
+
+    def __str__(self):
+        return "{}\n=====\n{}\nValue: {}\nDamage: {}".format(self.name, self.description, self.value, self.damage)
+
+
+class Gun(Weapon):
+    def __init__(self):
+        super().__init__(name="Gun",
+                         description="A Wepon useful for protection ",
+                         value=0,
+                         damage=5)
+
+
+class Dagger(Weapon):
+    def __init__(self):
+        super().__init__(name="Dagger",
+                         description="A small dagger with some rust. Somewhat more dangerous than a hand.",
+                         value=10,
+                         damage=10)
