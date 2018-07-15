ad7bd097c1d401704c57753c9c6136b8aa5abd95
nickthehacker <nickblackburn02@gmail.com>
2018-07-15 16:18:53 -0400

    addedgun

--- items.py
+++ items.py
@@ -7,3 +7,10 @@ class Item():
 
     def __str__(self):
         return "{}\n=====\n{}\nValue: {}\n".format(self.name, self.description, self.value)
+
+class Phone(Item):
+    def __init__(self, amt):
+        self.amt = amt
+        super().__init__(name="Cell-Phone",
+                         description=" {} stamped on the front.".format(str(self.amt)),
+                         value=self.amt)
