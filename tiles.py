0784cf49f26db0ce6753c8c870652bd99c6e3ec0
nickthehacker <nickblackburn02@gmail.com>
2018-07-15 16:26:09 -0400

    added

--- tiles.py
+++ tiles.py
@@ -1,6 +1,11 @@
-import items, enemies
+import items, Enemy
 
 class MapTile:
     def __init__(self, x, y):
         self.x = x
         self.y = y
+def intro_text(self):
+    raise NotImplementedError()
+
+def modify_player(self, player):
+    raise NotImplementedError()
